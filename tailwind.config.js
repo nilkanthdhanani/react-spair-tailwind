001299d47a94a66845a59e79059e82d2eb7feb8e
Nilkanth Dhanani <168087462+nilkanthdhanani@users.noreply.github.com>
2024-09-19 18:44:05 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   content: [
-    './src/modules/home/index.js',
-    './src/common/footer/index.js',
-    './src/common/header/index.js',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
